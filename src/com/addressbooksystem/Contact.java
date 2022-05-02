bab198f42c7d31c0338e5b71274f7cd87e483ea0
PavanKumar107 <99305975+PavanKumar107@users.noreply.github.com>
2022-05-02 15:09:25 +0530

    UC15

---
+++ src/com/addressbooksystem/Contact.java
@@ -0,0 +1,69 @@
+package com.addressbooksystem;
+
+public class Contact {
+
+	public String id;
+	public String firstName;
+	public String lastName;
+	public String address;
+	public String city;
+	public String state;
+	public int zip;
+	public int phoneNumber;
+	public String eMail;
+
+	public Contact(int i, String string, String string2, String string3, String string4, String string5, String string6, String string7, String string8) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Object getState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getFirstName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setFirstName(String next) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setAddress(String next) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setCity(String next) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setState(String next) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setZip(String next) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setPhoneNumber(String next) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void seteMail(String next) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setLastName(String next) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/com/addressbooksystem/GsonBuilder.java
@@ -0,0 +1,10 @@
+package com.addressbooksystem;
+
+public class GsonBuilder {
+
+	public Gson create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
